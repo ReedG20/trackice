5a005f1404105ef8960e3281a6a49ae6a2a84a68
ReedG20 <reedgrenager@gmail.com>
2026-01-20 20:15:36 -0500

    bad sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <main className="relative w-screen h-screen overflow-hidden">
       <Map />
-      <div className="absolute bottom-4 right-4 z-10">
+      <div className="absolute bottom-10 right-4 z-10">
         <ThemeToggle />
       </div>
     </main>
